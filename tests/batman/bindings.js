d55bff2104a977dfdcb0ea94e389095acfed5451
Nicholas Small <nick@shopify.com>
2011-03-07 18:39:41 -0500

    Experimental transactions support.

--- tests/batman/bindings.js
+++ tests/batman/bindings.js
@@ -158,4 +158,59 @@ Batman.ready(function() {
         equal(binding('foo'), 'foo', 'validator is removed');
     });
     
+    module('Batman.Transaction', {
+        setup: function() {
+            this.obj = Batman({
+                foo: $binding('bar'),
+                baz: $binding('qux')
+            })
+        },
+        
+        teardown: function() {
+            delete this.obj;
+        }
+    });
+    
+    test('creation', function() {
+        var t = Batman.transaction(this.obj);
+        equal(t.isTransaction, true, 'transaction is transaction');
+        
+        ok(!t.foo.isBinding, 'simple values are not bindings');
+        equal(t.foo, 'bar', 'has correct simple values');
+    });
+    
+    test('creation with arguments', function() {
+        var t = Batman.transaction(this.obj, {foo: 'foo'});
+        equal(t.foo, 'foo', 'arguments are applied');
+    });
+    
+    test('commit', function() {
+        var t = Batman.transaction(this.obj);
+        t.foo = 'foo';
+        t.newValue = 'pew pew';
+        t.commit();
+        
+        equal(this.obj.foo.isBinding, true, 'binding is still binding');
+        equal(this.obj.foo(), 'foo', 'binding value is applied');
+        equal(this.obj.newValue, 'pew pew', 'simple value is applied');
+    });
+    
+    test('transactionable', function() {
+        Batman.Transactionable.applyTo(this.obj);
+        equal(typeof this.obj.transaction, 'function', 'transaction method added');
+        
+        var t = this.obj.transaction();
+        equal(t.isTransaction, true, 'transaction is a transaction');
+        equal(t.foo, 'bar', 'simple value is correct');
+        
+        t.foo = 'foo';
+        t.commit();
+        
+        equal(this.obj.foo(), 'foo', 'binding value is set');
+    });
+    
+    test('change', function() {
+        
+    });
+    
 });
