1324f78fb1f571bc661ee383fd6f751159b89eff
Nicholas Small <nick@shopify.com>
2011-03-14 12:15:30 -0400

    Log errors in compiler.

--- tools/framework.js
+++ tools/framework.js
@@ -15,6 +15,8 @@ var js = files.map(function(file) { return '--js ' + Path.join(__dirname, '..', 
 console.log('compiling batman.js...')
 
 exec('java -jar ' + Path.join(__dirname, 'compiler.jar') + ' ' + js + ' --js_output_file ' + Path.join(process.cwd(), 'lib', 'batman-min.js'), function(err, stdout) {
-	if (!err)
-		console.log('batman.js compiled into lib/batman-min.js')
+	if (err)
+		return console.log(err.message)
+	
+	console.log('batman.js compiled into lib/batman-min.js')
 })
