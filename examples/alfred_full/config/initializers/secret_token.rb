51335927878bcf3e7d9d7f533fe7ec0085a87fd3
Nicholas Small <nick@shopify.com>
2011-03-17 17:46:43 -0400

    Simple Rails app (alfred_full) for Alfred REST API.

---
+++ examples/alfred_full/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alfred::Application.config.secret_token = '07b81c95c3b0d27385342e510e4b27c160c85787234fb9fee70a3caee77b79e59b36a9c8c1538b812aa5285d919be823397931211cb1c328bcfc6d42bcb3b684'
