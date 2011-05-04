bf97b1519117315f42948ab15263ab201762d8c6
Nicholas Small <nick@shopify.com>
2011-05-03 17:05:45 -0700

    Commit.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
 // Begin jquery tweet //
 $(document).ready(function(){
     $(".tweet").tweet({
-        username: "shopify",
+        query: "#batmanjs",
         join_text: "auto",
         count: 3,
-        query: "",
         auto_join_text_default: "", 
         auto_join_text_ed: "",
         auto_join_text_ing: "",
@@ -19,32 +18,4 @@ $(document).ready(function() {
   $('#sub #content-column-wrapper a, #sub #content-column-wrapper-full a, ').filter(function() {
     return this.hostname && this.hostname !== location.hostname;
   }).after('<img style="padding: 0 2px 0 3px;" src="images/icon-external-link.png" alt="external link"/>');
-});
-
-
-// Begin code snippet box //
-$(document).ready(function(){
-    $('pre.code').highlight({source:1, zebra:1, indent:'space', list:'ol'});
-});
-
-// Begin fancybox //
-$(document).ready(function() {
-
-    /* This is basic - uses default settings */
-    $("a#fancybox-image").fancybox();
-    
-    /* Using custom settings */
-    $("a#fancybox-content").fancybox({
-    	'hideOnContentClick': true
-    });
-    
-    /* Apply fancybox to multiple items */
-    $("a.fancybox-group").fancybox({
-    	'transitionIn'	:	'elastic',
-    	'transitionOut'	:	'elastic',
-    	'speedIn'		:	600, 
-    	'speedOut'		:	200, 
-    	'overlayShow'	:	false
-    });
-    
 });
