airhorns/batman

commit df18c4411362dc4abe7315ad04826e7e6d331a9f

Re-reintroduce data-read and data-write as data-target and data-source.
 - These were purposefully removed in b227af225654f, and then accidentally reintroduced in 4799d967689fa.
 - We now want to keep them, its decided.
 - data-source replaces what was data-write. data-source on a node specifies a key path. When the key path changes, the node will be updated with the new value. When the node changes, nothing will happen.
 - data-target replaces what was data-read. data-target on a node specifies a key path. When the node changes, the key path will be set to the new value of the node. When the key path changes, nothing will happen.
 - Oneway binding tests were also broken out into their own file.
 - References #89.

lib/batman.js
@@ -3500,7 +3500,7 @@
       return this._keyContext;
     });
     function Binding() {
-      var shouldSet;
+      var shouldSet, _ref2, _ref3;
       Binding.__super__.constructor.apply(this, arguments);
       this.parseFilter();
       this.nodeChange || (this.nodeChange = __bind(function(node, context) {
@@ -3512,18 +3512,20 @@
         return Batman.DOM.valueForNode(this.node, value);
       });
       shouldSet = true;
-      if (this.only !== 'write' && Batman.DOM.nodeIsEditable(this.node)) {
+      if (((_ref2 = this.only) === false || _ref2 === 'nodeChange') && Batman.DOM.nodeIsEditable(this.node)) {
         Batman.DOM.events.change(this.node, __bind(function() {
           shouldSet = false;
           this.nodeChange(this.node, this._keyContext || this.value, this);
           return shouldSet = true;
         }, this));
       }
-      this.observe('filteredValue', true, __bind(function(value) {
-        if (shouldSet && this.only !== 'read') {
-          return this.dataChange(value, this.node, this);
-        }
-      }, this));
+      if ((_ref3 = this.only) === false || _ref3 === 'dataChange') {
+        this.observe('filteredValue', true, __bind(function(value) {
+          if (shouldSet) {
+            return this.dataChange(value, this.node, this);
+          }
+        }, this));
+      }
       this;
     }
     Binding.prototype.parseFilter = function() {
@@ -3672,10 +3674,9 @@
         return this.pop();
       }, this));
     };
-    RenderContext.prototype.bind = function(only, node, key, dataChange, nodeChange) {
-      var _ref2;
-      if (!nodeChange && only && typeof only !== 'string') {
-        _ref2 = [only, node, key, dataChange], node = _ref2[0], key = _ref2[1], dataChange = _ref2[2], nodeChange = _ref2[3];
+    RenderContext.prototype.bind = function(node, key, dataChange, nodeChange, only) {
+      if (only == null) {
+        only = false;
       }
       return new Binding({
         renderContext: this,
@@ -3690,11 +3691,11 @@
   })();
   Batman.DOM = {
     readers: {
-      read: function(node, key, context, renderer) {
-        return Batman.DOM.readers.bind(node, key, context, renderer, 'read');
+      target: function(node, key, context, renderer) {
+        return Batman.DOM.readers.bind(node, key, context, renderer, 'nodeChange');
       },
-      write: function(node, key, context, renderer) {
-        return Batman.DOM.readers.bind(node, key, context, renderer, 'write');
+      source: function(node, key, context, renderer) {
+        return Batman.DOM.readers.bind(node, key, context, renderer, 'dataChange');
       },
       bind: function(node, key, context, renderer, only) {
         var _ref2, _ref3, _ref4;
@@ -3702,7 +3703,7 @@
           case 'input':
             switch (node.getAttribute('type')) {
               case 'checkbox':
-                return Batman.DOM.attrReaders.bind(node, 'checked', key, context, only);
+                return Batman.DOM.attrReaders.bind(node, 'checked', key, context, renderer, only);
               case 'radio':
                 return (_ref2 = Batman.DOM.binders).radio.apply(_ref2, arguments);
               case 'file':
@@ -3712,7 +3713,7 @@
           case 'select':
             return (_ref4 = Batman.DOM.binders).select.apply(_ref4, arguments);
         }
-        return context.bind(only, node, key);
+        return context.bind(node, key, void 0, void 0, only);
       },
       context: function(node, key, context) {
         return context.addKeyToScopeForNode(node, key);
@@ -3817,16 +3818,16 @@
         }
         return value;
       },
-      write: function(node, attr, key, context) {
-        return Batman.DOM.attrReaders.bind(node, attr, key, context, 'write');
+      source: function(node, attr, key, context, renderer) {
+        return Batman.DOM.attrReaders.bind(node, attr, key, context, renderer, 'dataChange');
       },
-      bind: function(node, attr, key, context, only) {
-        var contextChange, nodeChange;
+      bind: function(node, attr, key, context, renderer, only) {
+        var dataChange, nodeChange;
         switch (attr) {
           case 'checked':
           case 'disabled':
           case 'selected':
-            contextChange = function(value) {
+            dataChange = function(value) {
               var _base;
               node[attr] = !!value;
               return typeof (_base = Batman.data(node.parentNode, 'updateBinding')) === "function" ? _base() : void 0;
@@ -3840,7 +3841,7 @@
             });
             break;
           case 'value':
-            contextChange = function(value) {
+            dataChange = function(value) {
               return node.value = value;
             };
             nodeChange = function(node, subContext) {
@@ -3848,14 +3849,14 @@
             };
             break;
           default:
-            contextChange = function(value) {
+            dataChange = function(value) {
               return node.setAttribute(attr, value);
             };
             nodeChange = function(node, subContext) {
               return subContext.set(key, Batman.DOM.attrReaders._parseAttribute(node.getAttribute(attr)));
             };
         }
-        return context.bind(only, node, key, contextChange, nodeChange);
+        return context.bind(node, key, dataChange, nodeChange, only);
       },
       context: function(node, contextName, key, context) {
         return context.addKeyToScopeForNode(node, key, contextName);
@@ -4082,8 +4083,8 @@
           return _results;
         }, this);
         return renderer.rendered(function() {
-          var contextChange, nodeChange;
-          contextChange = function(newValue) {
+          var dataChange, nodeChange;
+          dataChange = function(newValue) {
             var child, match, matches, value, valueToChild, _j, _k, _l, _len2, _len3, _len4, _ref3, _ref4;
             if (newValue instanceof Array) {
               valueToChild = {};
@@ -4117,12 +4118,12 @@
             return updateOptionBindings();
           };
           Batman.data(node, 'updateBinding', updateSelectBinding);
-          return context.bind(only, node, key, contextChange, nodeChange);
+          return context.bind(node, key, dataChange, nodeChange, only);
         });
       },
       radio: function(node, key, context, renderer, only) {
-        var contextChange, nodeChange;
-        contextChange = function(value) {
+        var dataChange, nodeChange;
+        dataChange = function(value) {
           var boundValue, container, _ref2;
           _ref2 = context.findKey(key), boundValue = _ref2[0], container = _ref2[1];
           if (boundValue) {
@@ -4134,10 +4135,10 @@
         nodeChange = function(newNode, subContext) {
           return subContext.set(key, Batman.DOM.attrReaders._parseAttribute(node.value));
         };
-        return context.bind(only, node, key, contextChange, nodeChange);
+        return context.bind(node, key, dataChange, nodeChange, only);
       },
       file: function(node, key, context, renderer, only) {
-        return context.bind(only, node, key, function() {
+        return context.bind(node, key, function() {
           return developer.warn("Can't write to file inputs! Tried to on key " + key + ".");
         }, function(node, subContext) {
           var actualObject, adapter, _j, _len2, _ref2;
@@ -4160,7 +4161,7 @@
           } else {
             return subContext.set(key, node.files[0]);
           }
-        });
+        }, only);
       }
     },
     events: {
@@ -4289,25 +4290,25 @@
           if (isSetting) {
             return node.innerHTML = value;
           } else {
-            return node.innerHTML;
+            return node.innerHTMLa;
           }
       }
     },
     nodeIsEditable: function(node) {
       var _ref2;
       return (_ref2 = node.nodeName.toUpperCase()) === 'INPUT' || _ref2 === 'TEXTAREA' || _ref2 === 'SELECT';
-    }
+    },
+    addEventListener: $addEventListener = (typeof window !== "undefined" && window !== null ? window.addEventListener : void 0) ? (function(node, eventName, callback) {
+      return node.addEventListener(eventName, callback, false);
+    }) : (function(node, eventName, callback) {
+      return node.attachEvent("on" + eventName, callback);
+    }),
+    removeEventListener: $removeEventListener = (typeof window !== "undefined" && window !== null ? window.removeEventListener : void 0) ? (function(elem, eventType, handler) {
+      return elem.removeEventListener(eventType, handler, false);
+    }) : (function(elem, eventType, handler) {
+      return elem.detachEvent('on' + eventType, handler);
+    })
   };
-  Batman.DOM.addEventListener = $addEventListener = (typeof window !== "undefined" && window !== null ? window.addEventListener : void 0) ? (function(node, eventName, callback) {
-    return node.addEventListener(eventName, callback, false);
-  }) : (function(node, eventName, callback) {
-    return node.attachEvent("on" + eventName, callback);
-  });
-  Batman.DOM.removeEventListener = $removeEventListener = (typeof window !== "undefined" && window !== null ? window.removeEventListener : void 0) ? (function(elem, eventType, handler) {
-    return elem.removeEventListener(eventType, handler, false);
-  }) : (function(elem, eventType, handler) {
-    return elem.detachEvent('on' + eventType, handler);
-  });
   buntUndefined = function(f) {
     return function(value) {
       if (typeof value === 'undefined') {

src/batman.coffee
@@ -2587,15 +2587,17 @@ class Binding extends Batman.Object
     shouldSet = yes
 
     # And attach them.
-    if @only != 'write' and Batman.DOM.nodeIsEditable(@node)
+    if @only in [false, 'nodeChange'] and Batman.DOM.nodeIsEditable(@node)
       Batman.DOM.events.change @node, =>
         shouldSet = no
         @nodeChange(@node, @_keyContext || @value, @)
         shouldSet = yes
+
     # Observe the value of this binding's `filteredValue` and fire it immediately to update the node.
-    @observe 'filteredValue', yes, (value) =>
-      if shouldSet and @only != 'read'
-        @dataChange(value, @node, @)
+    if @only in [false, 'dataChange']
+      @observe 'filteredValue', yes, (value) =>
+        if shouldSet
+          @dataChange(value, @node, @)
     @
 
   parseFilter: ->
@@ -2742,12 +2744,9 @@ class RenderContext
   # creates a `Binding` to the key (supporting filters and the context stack), which fires the observers
   # when appropriate. Note that `Binding` has default observers for `dataChange` and `nodeChange` that
   # will set node/object values if these observers aren't passed in here.
-  # The optional `only` parameter can be used to create read-only or write-only bindings. If left unset,
-  # both read and write events are observed.
-  bind: (only, node, key, dataChange, nodeChange) ->
-    if !nodeChange and only and typeof only != 'string'
-      [node, key, dataChange, nodeChange] = [only, node, key, dataChange]
-
+  # The optional `only` parameter can be used to create data-to-node-only or node-to-data-only bindings. If left unset,
+  # both data-to-node (source) and node-to-data (target) events are observed.
+  bind: (node, key, dataChange, nodeChange, only = false) ->
     return new Binding
       renderContext: @
       keyPath: key
@@ -2760,18 +2759,18 @@ Batman.DOM = {
   # `Batman.DOM.readers` contains the functions used for binding a node's value or innerHTML, showing/hiding nodes,
   # and any other `data-#{name}=""` style DOM directives.
   readers: {
-    read: (node, key, context, renderer) ->
-      Batman.DOM.readers.bind(node, key, context, renderer, 'read')
+    target: (node, key, context, renderer) ->
+      Batman.DOM.readers.bind(node, key, context, renderer, 'nodeChange')
 
-    write: (node, key, context, renderer) ->
-      Batman.DOM.readers.bind(node, key, context, renderer, 'write')
+    source: (node, key, context, renderer) ->
+      Batman.DOM.readers.bind(node, key, context, renderer, 'dataChange')
 
     bind: (node, key, context, renderer, only) ->
       switch node.nodeName.toLowerCase()
         when 'input'
           switch node.getAttribute('type')
             when 'checkbox'
-              return Batman.DOM.attrReaders.bind(node, 'checked', key, context, only)
+              return Batman.DOM.attrReaders.bind(node, 'checked', key, context, renderer, only)
             when 'radio'
               return Batman.DOM.binders.radio(arguments...)
             when 'file'
@@ -2780,7 +2779,7 @@ Batman.DOM = {
           return Batman.DOM.binders.select(arguments...)
 
       # Fallback on the default nodeChange and dataChange observers in Binding
-      context.bind(only, node, key)
+      context.bind(node, key, undefined, undefined, only)
 
     context: (node, key, context) -> context.addKeyToScopeForNode(node, key)
 
@@ -2862,13 +2861,13 @@ Batman.DOM = {
       if value is 'true' then value = true
       value
 
-    write: (node, attr, key, context) ->
-      Batman.DOM.attrReaders.bind node, attr, key, context, 'write'
+    source: (node, attr, key, context, renderer) ->
+      Batman.DOM.attrReaders.bind node, attr, key, context, renderer, 'dataChange'
 
-    bind: (node, attr, key, context, only) ->
+    bind: (node, attr, key, context, renderer, only) ->
       switch attr
         when 'checked', 'disabled', 'selected'
-          contextChange = (value) ->
+          dataChange = (value) ->
             node[attr] = !!value
             # Update the parent's binding if necessary
             Batman.data(node.parentNode, 'updateBinding')?()
@@ -2882,13 +2881,13 @@ Batman.DOM = {
             key: key
 
         when 'value'
-          contextChange = (value) -> node.value = value
+          dataChange = (value) -> node.value = value
           nodeChange = (node, subContext) -> subContext.set(key, Batman.DOM.attrReaders._parseAttribute(node.value))
         else
-          contextChange = (value) -> node.setAttribute(attr, value)
+          dataChange = (value) -> node.setAttribute(attr, value)
           nodeChange = (node, subContext) -> subContext.set(key, Batman.DOM.attrReaders._parseAttribute(node.getAttribute(attr)))
 
-      context.bind(only, node, key, contextChange, nodeChange)
+      context.bind(node, key, dataChange, nodeChange, only)
 
     context: (node, contextName, key, context) -> context.addKeyToScopeForNode(node, key, contextName)
 
@@ -3063,7 +3062,7 @@ Batman.DOM = {
       # wait for the select to render before binding to it
       renderer.rendered ->
         # Update the select box with the binding's new value.
-        contextChange = (newValue) ->
+        dataChange = (newValue) ->
           # For multi-select boxes, the `value` property only holds the first
           # selection, so we need to go through the child options and update
           # as necessary.
@@ -3097,10 +3096,10 @@ Batman.DOM = {
         Batman.data node, 'updateBinding', updateSelectBinding
 
         # Create the binding
-        context.bind only, node, key, contextChange, nodeChange
+        context.bind node, key, dataChange, nodeChange, only
 
     radio: (node, key, context, renderer, only) ->
-      contextChange = (value) ->
+      dataChange = (value) ->
         # don't overwrite `checked` attributes in the HTML unless a bound
         # value is defined in the context. if no bound value is found, bind
         # to the key if the node is checked.
@@ -3111,10 +3110,10 @@ Batman.DOM = {
           container.set key, node.value
       nodeChange = (newNode, subContext) ->
         subContext.set(key, Batman.DOM.attrReaders._parseAttribute(node.value))
-      context.bind only, node, key, contextChange, nodeChange
+      context.bind node, key, dataChange, nodeChange, only
 
     file: (node, key, context, renderer, only) ->
-      context.bind(only, node, key, ->
+      context.bind(node, key, ->
         developer.warn "Can't write to file inputs! Tried to on key #{key}."
       , (node, subContext) ->
         if subContext instanceof RenderContext.BindingProxy
@@ -3129,7 +3128,7 @@ Batman.DOM = {
           subContext.set key, Array::slice.call(node.files)
         else
           subContext.set key, node.files[0]
-      )
+      , only)
   }
 
   # `Batman.DOM.events` contains the helpers used for binding to events. These aren't called by
@@ -3213,20 +3212,23 @@ Batman.DOM = {
       when 'SELECT'
         node.value = value
       else
-        if isSetting then (node.innerHTML = value) else node.innerHTML
+        if isSetting then (node.innerHTML = value) else node.innerHTMLa
+
   nodeIsEditable: (node) ->
     node.nodeName.toUpperCase() in ['INPUT', 'TEXTAREA', 'SELECT']
+
+  # `$addEventListener uses attachEvent when necessary
+  addEventListener: $addEventListener = if window?.addEventListener
+      ((node, eventName, callback) -> node.addEventListener eventName, callback, false)
+    else
+      ((node, eventName, callback) -> node.attachEvent "on#{eventName}", callback)
+
+  # `$removeEventListener` uses detachEvent when necessary
+  removeEventListener: $removeEventListener = if window?.removeEventListener
+      ((elem, eventType, handler) -> elem.removeEventListener eventType, handler, false)
+    else
+      ((elem, eventType, handler) -> elem.detachEvent 'on'+eventType, handler)
 }
-
-# `$addEventListener uses attachEvent when necessary
-Batman.DOM.addEventListener = $addEventListener = 
-  if window?.addEventListener then ((node, eventName, callback) -> node.addEventListener eventName, callback, false)
-  else ((node, eventName, callback) -> node.attachEvent "on#{eventName}", callback)
-
-# `$removeEventListener` uses detachEvent when necessary
-Batman.DOM.removeEventListener = $removeEventListener =
-  if window?.removeEventListener then ((elem, eventType, handler) -> elem.removeEventListener eventType, handler, false)
-  else ((elem, eventType, handler) -> elem.detachEvent 'on'+eventType, handler)
 
 # Filters
 # -------

tests/batman/view/simple_rendering_test.coffee
@@ -27,7 +27,7 @@ asyncTest 'it should bind undefined values as empty strings', 1, ->
     QUnit.start()
 
 asyncTest 'it should ignore empty bindings', 1, ->
-  helpers.render '<div data-bind=""></div>', Batman(), (node) =>
+  helpers.render '<div datdataChange-bind=""></div>', Batman(), (node) =>
     equals node.html(), ""
     QUnit.start()
 
@@ -395,63 +395,3 @@ asyncTest 'should bind to the value of radio buttons', ->
     helpers.triggerChange(fixed)
     delay =>
       equal context.get('ad.sale_type'), 'fixed'
-
-QUnit.module 'one-way bindings'
-
-asyncTest 'read should update only the binding value', 3, ->
-  source = '<input type="text" data-read="foo" value="start"/>'
-  context = Batman foo: null
-  helpers.render source, context, (node) ->
-    node = node[0]
-    equal node.value, 'start'
-    node.value = 'bar'
-    helpers.triggerChange node
-    delay =>
-      equal context.get('foo'), 'bar'
-      context.set 'foo', 'baz'
-      delay =>
-        equal node.value, 'bar'
-
-asyncTest 'write should update only the bound node', 3, ->
-  source = '<input type="text" data-write="foo" value="start"/>'
-  context = Batman foo: 'bar'
-  helpers.render source, context, (node) ->
-    node = node[0]
-    equal node.value, 'bar'
-    node.value = 'baz'
-    helpers.triggerChange node
-    delay =>
-      equal context.get('foo'), 'bar'
-      context.set 'foo', 'end'
-      delay =>
-        equal node.value, 'end'
-
-asyncTest 'attribute write should update only the bound attribute', 3, ->
-  source = '<input type="text" data-write-width="foo.width" value="start" width="10"/>'
-  context = Batman
-    foo: Batman
-      width: 20
-  helpers.render source, context, (node) ->
-    node = node[0]
-    equal node.getAttribute('width'), '20'
-    node.setAttribute 'width', 30
-    helpers.triggerChange node
-    delay =>
-      equal context.get('foo.width'), 20
-      context.set 'foo.width', 40
-      delay =>
-        equal node.getAttribute('width'), '40'
-
-asyncTest 'data-write and data-read work correctly on the same node', ->
-  source = '<input type="text" data-read="there" data-write="here" value="start"/>'
-  context = Batman here: 'here', there: ''
-  helpers.render source, context, (node) ->
-    node = node[0]
-    equal node.value, 'here'
-    equal context.get('there'), ''
-    node.value = 'there'
-    helpers.triggerChange node
-    delay =>
-      equal context.get('there'), 'there'
-      equal context.get('here'), 'here'
-

tests/batman/test.html
@@ -47,6 +47,7 @@
     <script type="text/coffeescript" src="view/view_helper.coffee"></script>
     <script type="text/coffeescript" src="view/view_test.coffee"></script>
     <script type="text/coffeescript" src="view/simple_rendering_test.coffee"></script>
+    <script type="text/coffeescript" src="view/oneway_binding_test.coffee"></script>
     <script type="text/coffeescript" src="view/loop_rendering_test.coffee"></script>
     <script type="text/coffeescript" src="view/nested_loop_rendering_test.coffee"></script>
     <script type="text/coffeescript" src="view/yield_and_contentfor_test.coffee"></script>

tests/batman/test_static.html
@@ -44,6 +44,7 @@
     <script type="text/javascript" src="view/view_helper.js"></script>
     <script type="text/javascript" src="view/view_test.js"></script>
     <script type="text/javascript" src="view/simple_rendering_test.js"></script>
+    <script type="text/javascript" src="view/oneway_binding_test.js"></script>
     <script type="text/javascript" src="view/loop_rendering_test.js"></script>
     <script type="text/javascript" src="view/nested_loop_rendering_test.js"></script>
     <script type="text/javascript" src="view/yield_and_contentfor_test.js"></script>

tests/batman/view/oneway_binding_test.coffee
@@ -1,0 +1,62 @@
+helpers = if typeof require is 'undefined' then window.viewHelpers else require './view_helper'
+
+QUnit.module 'Batman.View: one-way bindings:'
+
+asyncTest 'target should update only the javascript value', 3, ->
+  source = '<input type="text" data-target="foo" value="start"/>'
+  context = Batman foo: null
+  helpers.render source, context, (node) ->
+    node = node[0]
+    equal node.value, 'start'
+    node.value = 'bar'
+    helpers.triggerChange node
+    delay =>
+      equal context.get('foo'), 'bar'
+      context.set 'foo', 'baz'
+      delay =>
+        equal node.value, 'bar'
+
+asyncTest 'source should update only the bound node', 3, ->
+  source = '<input type="text" data-source="foo" value="start"/>'
+  context = Batman foo: 'bar'
+  helpers.render source, context, (node) ->
+    node = node[0]
+    equal node.value, 'bar'
+    node.value = 'baz'
+    helpers.triggerChange node
+    delay =>
+      equal context.get('foo'), 'bar'
+      context.set 'foo', 'end'
+      delay =>
+        equal node.value, 'end'
+
+asyncTest 'attribute source should update only the bound attribute on the node', 3, ->
+  source = '<input type="text" data-source-width="foo.width" value="start" width="10"/>'
+  context = Batman
+    foo: Batman
+      width: 20
+  helpers.render source, context, (node) ->
+    node = node[0]
+    equal node.getAttribute('width'), '20'
+    node.setAttribute 'width', 30
+    helpers.triggerChange node
+    delay =>
+      equal context.get('foo.width'), 20 # nodeChange has no effect
+      context.set 'foo.width', 40
+      delay =>
+        equal node.getAttribute('width'), '40'
+
+asyncTest 'data-source and data-target work correctly on the same node', ->
+  source = '<input type="text" data-target="there" data-source="here" value="start"/>'
+  context = Batman here: 'here', there: ''
+  helpers.render source, context, (node) ->
+    node = node[0]
+    equal node.value, 'here'
+    equal context.get('there'), ''
+    node.value = 'there'
+    helpers.triggerChange node
+    delay =>
+      equal context.get('there'), 'there'
+      equal context.get('here'), 'here'
+
+

commit 54b257a997bee9edf7a6cf9ab98b758c721fdf76

Fix typos introduced in df18c44. Sorry everyone, going too fast.

lib/batman.js
@@ -4337,7 +4337,7 @@
           if (isSetting) {
             return node.innerHTML = value;
           } else {
-            return node.innerHTMLa;
+            return node.innerHTML;
           }
       }
     },

src/batman.coffee
@@ -3224,7 +3224,7 @@ Batman.DOM = {
       when 'SELECT'
         node.value = value
       else
-        if isSetting then (node.innerHTML = value) else node.innerHTMLa
+        if isSetting then (node.innerHTML = value) else node.innerHTML
 
   nodeIsEditable: (node) ->
     node.nodeName.toUpperCase() in ['INPUT', 'TEXTAREA', 'SELECT']

tests/batman/view/simple_rendering_test.coffee
@@ -27,7 +27,7 @@ asyncTest 'it should bind undefined values as empty strings', 1, ->
     QUnit.start()
 
 asyncTest 'it should ignore empty bindings', 1, ->
-  helpers.render '<div datdataChange-bind=""></div>', Batman(), (node) =>
+  helpers.render '<div data-bind=""></div>', Batman(), (node) =>
     equals node.html(), ""
     QUnit.start()
 