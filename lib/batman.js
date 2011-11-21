airhorns/batman

commit 247dea300af4ce10956eab036aed810f831efe1a

Make showif and hide if use !important declarations so !importants in stylesheets don't override them.

lib/batman.js
@@ -273,7 +273,9 @@
       }
     },
     "do": function(f) {
-      return f();
+      if (!developer.suppressed) {
+        return f();
+      }
     },
     addFilters: function() {
       return $mixin(Batman.Filters, {
@@ -582,7 +584,7 @@
         var keyType;
         keyType = $typeOf(this.key);
         if (keyType === 'Array' || keyType === 'Object') {
-          return developer.warn("Constructing a property with an " + keyType + " for a key:", this);
+          return developer.log("Accessing a property with an " + keyType + " key. This is okay, but could be a source of memory leaks if you aren't careful.");
         }
       }, this));
     }
@@ -5631,7 +5633,7 @@
         if (typeof hide === 'function') {
           return hide.call(this.node);
         } else {
-          return this.node.style.display = 'none';
+          return this.node.style.display = 'none !important';
         }
       }
     };

src/batman.coffee
@@ -3844,7 +3844,7 @@ class Batman.DOM.ShowHideBinding extends Batman.DOM.AbstractBinding
       if typeof hide == 'function'
         hide.call @node
       else
-        @node.style.display = 'none'
+        @node.style.display = 'none !important'
 
 class Batman.DOM.CheckedBinding extends Batman.DOM.NodeAttributeBinding
   dataChange: (value) ->

tests/batman/view/filter_execution_test.coffee
@@ -155,10 +155,10 @@ asyncTest 'sort', 1, ->
     QUnit.start()
 
 asyncTest 'not', 1, ->
-  helpers.render '<div data-showif="foo | not"></div>',
+  helpers.render '<input type="checkbox" data-bind="foo | not" />',
     foo: true
   , (node) ->
-    equals node[0].style.display, "none"
+    equals node[0].checked, false
     QUnit.start()
 
 

tests/batman/view/simple_rendering_test.coffee
@@ -104,7 +104,10 @@ asyncTest 'it should allow visibility to be bound on block elements', 2, ->
       foo: false
     , (node) ->
         helpers.withNodeInDom node, ->
-          equal node.css('display'), 'none'
+          if IN_NODE
+            equal node.css('display'), 'none !important'
+          else
+            equal node.css('display'), 'none'
         QUnit.start()
 
 asyncTest 'it should allow visibility to be bound on inline elements', 2, ->
@@ -124,7 +127,10 @@ asyncTest 'it should allow visibility to be bound on inline elements', 2, ->
       foo: false
     , (node) ->
         helpers.withNodeInDom node, ->
-          equal node.css('display'), 'none'
+          if IN_NODE
+            equal node.css('display'), 'none !important'
+          else
+            equal node.css('display'), 'none'
         QUnit.start()
 
 asyncTest 'it should allow arbitrary attributes to be bound', 2, ->

commit 6128fe8c5a6ad25049d8ed7c9d29041d81f81b09

Fix an issue introduced in 247dea3 where data-showif and data-hideif stopped working in FF and IE.
 - Added a $setStyleProperty helper which works cross browser to set a style value with importance.

lib/batman.js
@@ -1,5 +1,5 @@
 (function() {
-  var $addEventListener, $appendChild, $clearImmediate, $contains, $extendsEnumerable, $forEach, $forgetParseExit, $functionName, $get, $hasAddEventListener, $insertBefore, $isChildOf, $mixin, $objectHasKey, $onParseExit, $passError, $preventDefault, $redirect, $removeEventListener, $removeNode, $setImmediate, $setInnerHTML, $trackBinding, $typeOf, $unbindNode, $unbindTree, $unmixin, Batman, BatmanObject, Validators, buntUndefined, camelize_rx, capitalize_rx, developer, div, filters, helpers, isEmptyDataObject, k, mixins, t, underscore_rx1, underscore_rx2, _Batman, _i, _implementImmediates, _len, _objectToString, _ref, _stateMachine_setState;
+  var $addEventListener, $appendChild, $clearImmediate, $contains, $extendsEnumerable, $forEach, $forgetParseExit, $functionName, $get, $hasAddEventListener, $insertBefore, $isChildOf, $mixin, $objectHasKey, $onParseExit, $passError, $preventDefault, $redirect, $removeEventListener, $removeNode, $setImmediate, $setInnerHTML, $setStyleProperty, $trackBinding, $typeOf, $unbindNode, $unbindTree, $unmixin, Batman, BatmanObject, Validators, buntUndefined, camelize_rx, capitalize_rx, developer, div, filters, helpers, isEmptyDataObject, k, mixins, t, underscore_rx1, underscore_rx2, _Batman, _i, _implementImmediates, _len, _objectToString, _ref, _stateMachine_setState;
   var __slice = Array.prototype.slice, __hasProp = Object.prototype.hasOwnProperty, __indexOf = Array.prototype.indexOf || function(item) {
     for (var i = 0, l = this.length; i < l; i++) {
       if (this[i] === item) return i;
@@ -5456,6 +5456,13 @@
       $unbindTree(node, false);
       return node != null ? node.innerHTML = html : void 0;
     },
+    setStyleProperty: $setStyleProperty = function(node, property, value, importance) {
+      if (node.style.setAttribute) {
+        return node.style.setAttribute(property, value, importance);
+      } else {
+        return node.style.setProperty(property, value, importance);
+      }
+    },
     removeNode: $removeNode = function(node) {
       var _ref;
       if ((_ref = node.parentNode) != null) {
@@ -5836,7 +5843,7 @@
         if (typeof hide === 'function') {
           return hide.call(this.node);
         } else {
-          return this.node.style.display = 'none !important';
+          return $setStyleProperty(this.node, 'display', 'none', 'important');
         }
       }
     };

src/batman.coffee
@@ -1596,7 +1596,7 @@ class Batman.Route extends Batman.Object
 
   dispatch: (url) ->
     params = @parameterize url
-    
+
     @dispatcher.app.get('currentParams').replace(params)
 
     $redirect('/404') if not (action = params.action) and url isnt '/404'
@@ -1693,7 +1693,7 @@ class Batman.Dispatcher extends Batman.Object
 
     @app.set 'currentURL', url
     @app.set 'currentRoute', route
-    
+
     url
 
 class Batman.Navigator
@@ -3650,6 +3650,12 @@ Batman.DOM = {
     $unbindTree node, false
     node?.innerHTML = html
 
+  setStyleProperty: $setStyleProperty = (node, property, value, importance) ->
+    if node.style.setAttribute
+      node.style.setAttribute(property, value, importance)
+    else
+      node.style.setProperty(property, value, importance)
+
   # Memory-safe removal of a node from the DOM
   removeNode: $removeNode = (node) ->
     node.parentNode?.removeChild node
@@ -3961,7 +3967,7 @@ class Batman.DOM.ShowHideBinding extends Batman.DOM.AbstractBinding
       if typeof hide == 'function'
         hide.call @node
       else
-        @node.style.display = 'none !important'
+        $setStyleProperty(@node, 'display', 'none', 'important')
 
 class Batman.DOM.CheckedBinding extends Batman.DOM.NodeAttributeBinding
   dataChange: (value) ->