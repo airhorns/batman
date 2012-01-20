airhorns/batman

commit dd20c2f4e2e096a290ea02160e6f9d1399dc50a4

Call view[Will/Did][Appear/Disappear] during yields.

lib/batman.js
@@ -5215,12 +5215,14 @@
           this.hasContainer = true;
           this.node = document.createElement('div');
           $setInnerHTML(this.node, html);
+          Batman.data(this.node.children[0], 'view', this);
         }
         return this.node;
       },
       set: function(_, node) {
         var updateHTML;
         this.node = node;
+        Batman.data(this.node, 'view', this);
         updateHTML = __bind(function(html) {
           if (html != null) {
             $setInnerHTML(this.get('node'), html);
@@ -5798,15 +5800,40 @@
         node = node.cloneNode(true);
       }
       yieldFn = function(yieldingNode) {
+        var child, view, _j, _len2, _ref2;
         if (_replaceContent || !Batman._data(yieldingNode, 'yielded')) {
+          _ref2 = yieldingNode.children;
+          for (_j = 0, _len2 = _ref2.length; _j < _len2; _j++) {
+            child = _ref2[_j];
+            Batman.DOM.didRemoveNode(child);
+          }
           $setInnerHTML(yieldingNode, '', true);
         }
         if (_yieldChildren) {
           while (node.childNodes.length > 0) {
+            child = node.childNodes[0];
+            view = Batman.data(child, 'view');
+            if (view != null) {
+              if (typeof view.viewWillAppear === "function") {
+                view.viewWillAppear(child);
+              }
+            }
             $appendChild(yieldingNode, node.childNodes[0], true);
+            if (view != null) {
+              if (typeof view.viewDidAppear === "function") {
+                view.viewDidAppear(child);
+              }
+            }
           }
         } else {
+          view = Batman.data(node, 'view');
+          if (view != null) {
+            view.viewWillAppear(child);
+          }
           $appendChild(yieldingNode, node, true);
+          if (view != null) {
+            view.viewDidAppear(child);
+          }
         }
         Batman._data(node, 'yielded', true);
         Batman._data(yieldingNode, 'yielded', true);

src/batman.coffee
@@ -3465,9 +3465,11 @@ class Batman.View extends Batman.Object
         @hasContainer = true
         @node = document.createElement 'div'
         $setInnerHTML(@node, html)
+        Batman.data(@node.children[0], 'view', @)
       return @node
     set: (_, node) ->
       @node = node
+      Batman.data(@node, 'view', @)
       updateHTML = (html) =>
         if html?
           $setInnerHTML(@get('node'), html)
@@ -3888,13 +3890,25 @@ Batman.DOM = {
 
     yieldFn = (yieldingNode) ->
       if _replaceContent || !Batman._data(yieldingNode, 'yielded')
+        Batman.DOM.didRemoveNode(child) for child in yieldingNode.children
         $setInnerHTML yieldingNode, '', true
 
       if _yieldChildren
         while node.childNodes.length > 0
+          child = node.childNodes[0]
+          view = Batman.data(child, 'view')
+          view?.viewWillAppear?(child)
+
           $appendChild yieldingNode, node.childNodes[0], true
+
+          view?.viewDidAppear?(child)
       else
+        view = Batman.data(node, 'view')
+        view?.viewWillAppear(child)
+
         $appendChild yieldingNode, node, true
+
+        view?.viewDidAppear(child)
 
       Batman._data node, 'yielded', true
       Batman._data yieldingNode, 'yielded', true

commit 4a500605cda7b1af8b63219f4e346335d2a4f026

Check for children
(Broken in dd20c2f4e2e096a290ea02160e6f9d1399dc50a4)

lib/batman.js
@@ -5500,7 +5500,9 @@
           this.hasContainer = true;
           this.node = document.createElement('div');
           $setInnerHTML(this.node, html);
-          Batman.data(this.node.children[0], 'view', this);
+          if (this.node.children.length > 0) {
+            Batman.data(this.node.children[0], 'view', this);
+          }
         }
         return this.node;
       },

src/batman.coffee
@@ -3635,7 +3635,8 @@ class Batman.View extends Batman.Object
         @hasContainer = true
         @node = document.createElement 'div'
         $setInnerHTML(@node, html)
-        Batman.data(@node.children[0], 'view', @)
+        if @node.children.length > 0
+          Batman.data(@node.children[0], 'view', @)
       return @node
     set: (_, node) ->
       @node = node