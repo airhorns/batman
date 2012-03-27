airhorns/batman

commit c34567cc9a115971a41d84cbd0ee635d286e0e62

Minor optimizations to parseNode.
 - Unroll CoffeeScript loop assignment to avoid function call
 - Use split instead of substr when pulling out bindings

lib/batman.js
@@ -6847,7 +6847,7 @@
     };
 
     Renderer.prototype.parseNode = function(node) {
-      var attr, bindings, key, nextNode, oldContext, readerArgs, result, skipChildren, varIndex, _base, _base2, _k, _len4, _name, _name2, _ref3;
+      var argument, attribute, bindings, keypath, names, nextNode, oldContext, result, skipChildren, _base, _base2, _k, _l, _len4, _len5, _ref3, _ref4, _ref5, _ref6;
       var _this = this;
       if (this.deferEvery && (new Date - this.startTime) > this.deferEvery) {
         this.resumeNode = node;
@@ -6855,27 +6855,18 @@
         return;
       }
       if (node.getAttribute && node.attributes) {
-        bindings = (function() {
-          var _k, _len4, _ref3, _ref4, _results;
-          _ref3 = node.attributes;
-          _results = [];
-          for (_k = 0, _len4 = _ref3.length; _k < _len4; _k++) {
-            attr = _ref3[_k];
-            name = (_ref4 = attr.nodeName.match(bindingRegexp)) != null ? _ref4[1] : void 0;
-            if (!name) continue;
-            if (~(varIndex = name.indexOf('-'))) {
-              _results.push([name.substr(0, varIndex), name.substr(varIndex + 1), attr.value]);
-            } else {
-              _results.push([name, attr.value]);
-            }
-          }
-          return _results;
-        })();
-        _ref3 = bindings.sort(this._sortBindings);
+        bindings = [];
+        _ref3 = node.attributes;
         for (_k = 0, _len4 = _ref3.length; _k < _len4; _k++) {
-          readerArgs = _ref3[_k];
-          key = readerArgs[1];
-          result = readerArgs.length === 2 ? typeof (_base = Batman.DOM.readers)[_name = readerArgs[0]] === "function" ? _base[_name](node, key, this.context, this) : void 0 : typeof (_base2 = Batman.DOM.attrReaders)[_name2 = readerArgs[0]] === "function" ? _base2[_name2](node, key, readerArgs[2], this.context, this) : void 0;
+          attribute = _ref3[_k];
+          name = (_ref4 = attribute.nodeName.match(bindingRegexp)) != null ? _ref4[1] : void 0;
+          if (!name) continue;
+          bindings.push((names = name.split('-')).length > 1 ? [names[0], names[1], attribute.value] : [name, void 0, attribute.value]);
+        }
+        _ref5 = bindings.sort(this._sortBindings);
+        for (_l = 0, _len5 = _ref5.length; _l < _len5; _l++) {
+          _ref6 = _ref5[_l], name = _ref6[0], argument = _ref6[1], keypath = _ref6[2];
+          result = argument ? typeof (_base = Batman.DOM.attrReaders)[name] === "function" ? _base[name](node, argument, keypath, this.context, this) : void 0 : typeof (_base2 = Batman.DOM.readers)[name] === "function" ? _base2[name](node, keypath, this.context, this) : void 0;
           if (result === false) {
             skipChildren = true;
             break;

src/batman.coffee
@@ -4117,20 +4117,20 @@ class Batman.Renderer extends Batman.Object
       @timeout = $setImmediate @resume
       return
     if node.getAttribute and node.attributes
-      bindings = for attr in node.attributes
-        name = attr.nodeName.match(bindingRegexp)?[1]
+      bindings = []
+      for attribute in node.attributes
+        name = attribute.nodeName.match(bindingRegexp)?[1]
         continue if not name
-        if ~(varIndex = name.indexOf('-'))
-          [name.substr(0, varIndex), name.substr(varIndex + 1), attr.value]
+        bindings.push if (names = name.split('-')).length > 1
+          [names[0], names[1], attribute.value]
         else
-          [name, attr.value]
+          [name, undefined, attribute.value]
 
-      for readerArgs in bindings.sort(@_sortBindings)
-        key = readerArgs[1]
-        result = if readerArgs.length == 2
-          Batman.DOM.readers[readerArgs[0]]?(node, key, @context, @)
+      for [name, argument, keypath] in bindings.sort(@_sortBindings)
+        result = if argument
+          Batman.DOM.attrReaders[name]?(node, argument, keypath, @context, @)
         else
-          Batman.DOM.attrReaders[readerArgs[0]]?(node, key, readerArgs[2], @context, @)
+          Batman.DOM.readers[name]?(node, keypath, @context, @)
 
         if result is false
           skipChildren = true

tests/prof/reports/View Rendering/Raw Node Parsing.coffee
@@ -1,0 +1,18 @@
+helpers = require 'sha_summarizer'
+
+keys = ['parseNode function: 1000 divs without bindings', 'parseNode function: 1000 divs with one binding', 'parseNode function: 1000 divs with 10 bindings', 'parseNode function: 1000 deeply nested divs', 'parseNode function: 1000 deeply nested divs with one binding', 'parseNode function: 1000 deeply nested divs with 10 bindings']
+
+shas = helpers.getAvailableShas(keys)
+
+header "No Bindings"
+barchart helpers.reportKeysAcrossShas([keys[0], keys[3]], shas)
+
+header "One Binding"
+barchart helpers.reportKeysAcrossShas([keys[1], keys[4]], shas)
+
+header "10 Bindings"
+barchart helpers.reportKeysAcrossShas([keys[2], keys[5]], shas)
+
+header "10 Attribute Bindings"
+barchart helpers.reportKeysAcrossShas([keys[2], keys[5]], shas)
+

tests/prof/tests/view/parsing_trees_performance.coffee
@@ -1,0 +1,175 @@
+Batman = require '../../../../lib/batman'
+Watson = require 'watson'
+jsdom = require 'jsdom'
+
+global.window = jsdom.jsdom("<html><head><script></script></head><body></body></html>").createWindow()
+global.window.Benchmark = Watson.Benchmark
+global.document = window.document
+
+Batman.Renderer::deferEvery = 0
+
+div = (text) ->
+  node = document.createElement('div')
+  node.innerHTML = text if text?
+  node
+
+Watson.benchmark 'parseNode function', (error, suite) ->
+  throw error if error
+
+  do ->
+    source = ("<div></div>" for i in [1..1000]).join('')
+    node = div(source)
+    context = Batman.RenderContext.base
+
+    suite.add '1000 divs without bindings', (deferred) ->
+      renderer = new Batman.Renderer(node, (->), context, {})
+      Batman.clearImmediate renderer.immediate
+      renderer.start()
+
+  do ->
+    source = ('<div data-test="yes"></div>' for i in [1..1000]).join('')
+    node = div(source)
+    context = Batman.RenderContext.base
+
+    Batman.DOM.readers.test = -> true
+
+    suite.add '1000 divs with one binding', (deferred) ->
+      renderer = new Batman.Renderer(node, (->), context, {})
+      Batman.clearImmediate renderer.immediate
+      renderer.start()
+
+  do ->
+    divSource = '<div ' + ("data-test#{i}=\"yes\"" for i in [1..10]).join(' ') + "></div>"
+    source = (divSource for i in [1..1000]).join('')
+    node = div(source)
+    context = Batman.RenderContext.base
+
+    for i in [1..10]
+      Batman.DOM.readers["test#{i}"] = -> true
+
+    suite.add '1000 divs with 10 bindings', (deferred) ->
+      renderer = new Batman.Renderer(node, (->), context, {})
+      Batman.clearImmediate renderer.immediate
+      renderer.start()
+
+  do ->
+    divSource = '<div ' + ("data-test#{i}-test=\"yes\"" for i in [1..10]).join(' ') + "></div>"
+    source = (divSource for i in [1..1000]).join('')
+    node = div(source)
+    context = Batman.RenderContext.base
+
+    for i in [1..10]
+      Batman.DOM.attrReaders["test#{i}"] = -> true
+
+    suite.add '1000 divs with 10 attribute bindings', (deferred) ->
+      renderer = new Batman.Renderer(node, (->), context, {})
+      Batman.clearImmediate renderer.immediate
+      renderer.start()
+
+  do ->
+    node = div()
+    for i in [1..10]
+      childA = div()
+      for j in [1..5]
+        childB = div()
+        for k in [1..10]
+          childC = div()
+          for l in [1..2]
+            childC.appendChild div()
+          childB.appendChild childC
+        childA.appendChild childB
+      node.appendChild childA
+
+    context = Batman.RenderContext.base
+
+    suite.add '1000 deeply nested divs', (deferred) ->
+      renderer = new Batman.Renderer(node, (->), context, {})
+      Batman.clearImmediate renderer.immediate
+      renderer.start()
+
+  do ->
+    divWithBinding = ->
+      x = div(arguments...)
+      x.setAttribute('data-test', 'yes')
+      x
+
+    node = divWithBinding()
+    for i in [1..10]
+      childA = divWithBinding()
+      for j in [1..5]
+        childB = divWithBinding()
+        for k in [1..10]
+          childC = divWithBinding()
+          for l in [1..2]
+            childC.appendChild divWithBinding()
+          childB.appendChild childC
+        childA.appendChild childB
+      node.appendChild childA
+
+    context = Batman.RenderContext.base
+    Batman.DOM.readers.test = -> true
+
+    suite.add '1000 deeply nested divs with one binding', (deferred) ->
+      renderer = new Batman.Renderer(node, (->), context, {})
+      Batman.clearImmediate renderer.immediate
+      renderer.start()
+
+  do ->
+    divWithBinding = ->
+      x = div(arguments...)
+      for i in [1..10]
+        x.setAttribute("data-test#{i}", 'yes')
+      x
+
+    node = divWithBinding()
+    for i in [1..10]
+      childA = divWithBinding()
+      for j in [1..5]
+        childB = divWithBinding()
+        for k in [1..10]
+          childC = divWithBinding()
+          for l in [1..2]
+            childC.appendChild divWithBinding()
+          childB.appendChild childC
+        childA.appendChild childB
+      node.appendChild childA
+
+    context = Batman.RenderContext.base
+    for i in [1..10]
+      Batman.DOM.readers["test#{i}"] = -> true
+
+    suite.add '1000 deeply nested divs with 10 bindings', (deferred) ->
+      renderer = new Batman.Renderer(node, (->), context, {})
+      Batman.clearImmediate renderer.immediate
+      renderer.start()
+
+  do ->
+    divWithBinding = ->
+      x = div(arguments...)
+      for i in [1..10]
+        x.setAttribute("data-test#{i}-test", 'yes')
+      x
+
+    node = divWithBinding()
+    for i in [1..10]
+      childA = divWithBinding()
+      for j in [1..5]
+        childB = divWithBinding()
+        for k in [1..10]
+          childC = divWithBinding()
+          for l in [1..2]
+            childC.appendChild divWithBinding()
+          childB.appendChild childC
+        childA.appendChild childB
+      node.appendChild childA
+
+    context = Batman.RenderContext.base
+    for i in [1..10]
+      Batman.DOM.attrReaders["test#{i}"] = -> true
+
+    suite.add '1000 deeply nested divs with 10 attribute bindings', (deferred) ->
+      renderer = new Batman.Renderer(node, (->), context, {})
+      Batman.clearImmediate renderer.immediate
+      renderer.start()
+
+  suite.run()

commit 0e11ad3f14c90e6660bfa9f2936ce01ea775b416

Fix data-bind-*-* attributes bug introduced by c34567c

lib/batman.js
@@ -6877,7 +6877,7 @@
           attribute = _ref3[_k];
           name = (_ref4 = attribute.nodeName.match(bindingRegexp)) != null ? _ref4[1] : void 0;
           if (!name) continue;
-          bindings.push((names = name.split('-')).length > 1 ? [names[0], names[1], attribute.value] : [name, void 0, attribute.value]);
+          bindings.push((names = name.split('-')).length > 1 ? [names[0], names.slice(1, names.length + 1 || 9e9).join('-'), attribute.value] : [name, void 0, attribute.value]);
         }
         _ref5 = bindings.sort(this._sortBindings);
         for (_l = 0, _len5 = _ref5.length; _l < _len5; _l++) {

src/batman.coffee
@@ -4110,7 +4110,7 @@ class Batman.Renderer extends Batman.Object
         name = attribute.nodeName.match(bindingRegexp)?[1]
         continue if not name
         bindings.push if (names = name.split('-')).length > 1
-          [names[0], names[1], attribute.value]
+          [names[0], names[1..names.length].join('-'), attribute.value]
         else
           [name, undefined, attribute.value]
 